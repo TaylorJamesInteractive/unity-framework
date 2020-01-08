8d717b1eed435387510b570e7c5b2ed96c282e99
pedrotj <pedro.garlaschi@taylorjames.com>
2020-01-08 11:19:51 +0000

    EventManager

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Collection of helpers to speed up the daily workflow.
 
 ## Installation
-If you are using the unity 2019.2 or above you should be able to add a `git` package to your packagemanager. so add the `https://github.com/TaylorJamesInteractive/unity-framework.git` URL.
+If you are using the unity `2019.2` or above you should be able to add a `git` package to your packagemanager. so add the `https://github.com/TaylorJamesInteractive/unity-framework.git` URL.
 
 Otherwise download or clone the repo and pull the content of the `/Runtime` folder.
 
---
+++ Runtime/Events/EventManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace com.tj.Events
+{
+    public class EventManager<T> : MonoBehaviour where T : BaseEvent
+    {
+        public delegate void OnEventHandlerDelegate(T evt);
+
+        private static Dictionary<System.Type , List<OnEventHandlerDelegate>> listeners;
+        private static Dictionary<System.Type, List<OnEventHandlerDelegate>> Listeners
+        {
+            get
+            {
+                if (listeners == null)
+                    listeners = new Dictionary<System.Type, List<OnEventHandlerDelegate>>();
+
+                return listeners;
+            }
+        }
+
+        public static void RegisterListener(OnEventHandlerDelegate listener) 
+        {
+            if (!Listeners.ContainsKey(typeof(T)))
+                Listeners[typeof(T)] = new List<OnEventHandlerDelegate>();
+
+              Listeners[typeof(T)].Add(listener);
+        }
+
+        public static void RemoveListener(OnEventHandlerDelegate listener)
+        {
+            if (!Listeners.ContainsKey(typeof(T)))
+                return;
+
+            Listeners[typeof(T)].Remove(listener);
+
+        }
+
+        public static void DispatchEvent(T evt)
+        {
+            foreach (OnEventHandlerDelegate listener in Listeners[typeof(T)])
+                listener.Invoke(evt);
+        }
+
+    }
+}
